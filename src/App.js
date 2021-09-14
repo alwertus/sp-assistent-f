670d81011e87c1a07c05de02378805a3f0b2a004
Alwertus <alwertus@gmail.com>
2021-09-15 02:31:33 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <h2>ZZZZ</h2>
+        <a href="/spassistent">clickme</a>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
